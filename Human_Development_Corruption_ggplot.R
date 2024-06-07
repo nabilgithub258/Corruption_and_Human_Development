8b59c1b5084e37c33a10793a0c941eba0eb0f900
nabilgithub258 <nabilmomin1989@gmail.com>
2024-06-07 14:28:04 -0400

    Add files via upload
    This graph was build in R studio using R programming language and with the aid of ggplot and ggploty.
    There is two variation to the graph, one being done with ggplot and the other one being done by ggplotly. In short one is more interactive thanks to ggplotly while the other is more static.
    The graph gives us an insight into how the human development is affected when the corruption scale goes higher and which countries are mostly a victim of this.

---
+++ Human_Development_Corruption_ggplot.R
@@ -0,0 +1,25 @@
+library(ggplot2movies)
+library(ggplot2)
+library(ggthemes)
+library(plotly)
+
+pointsToLabel <- c("Russia", "Venezuela", "Iraq", "Myanmar", "Sudan",
+                   "Afghanistan", "Congo", "Greece", "Argentina", "Brazil",
+                   "India", "Italy", "China", "South Africa", "Spane",
+                   "Botswana", "Cape Verde", "Bhutan", "Rwanda", "France",
+                   "United States", "Germany", "Britain", "Barbados", "Norway", "Japan",
+                   "New Zealand", "Singapore")
+
+df <- read_csv('Economist_Assignment_Data.csv')
+
+pl <- ggplot(df,aes(x=CPI,y=HDI,color=Region)) + geom_point(size=4,shape=1)
+
+pl2 <- pl + geom_smooth(aes(group = 1), method ='lm',formula = y ~ log(x),color='red',se=FALSE)
+
+pl3 <- pl2 + geom_text(aes(label = Country),color='grey20',data=subset(df,Country %in% pointsToLabel),check_overlap = TRUE)
+
+pl4 <- pl3 + theme_economist_white()
+
+pl5 <- pl4 + xlab('Corruption Perception Index') + ylab('Human Development Index') + ggtitle('Corruption and Human Development')
+
+print(ggplotly(pl5))
